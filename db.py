f27779a7ef278081e198bf7c9a09cd968b814ffa
vipplavai <vipplavai@gmail.com>
2025-03-02 15:30:51 +0530

    7th

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
 import pymongo
 import streamlit as st
+from pymongo.server_api import ServerApi
 
 # Secure MongoDB connection using Streamlit Secrets
 @st.cache_resource
 def init_connection():
-    return pymongo.MongoClient(st.secrets["mongo"]["uri"])
+    return pymongo.MongoClient(st.secrets["mongo"]["uri"], server_api=ServerApi('1'))
 
 # Initialize DB connection
 client = init_connection()
-db = client["Q_and_A"]  # Database name
-collection = db["content_data"]  # Collection name
+db = client["Q_and_A"]  # Your database name
+collection = db["content_data"]  # Your collection name
